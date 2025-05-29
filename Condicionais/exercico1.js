3a82c97f10c0b617813656063e30d69413904d86
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-05-29 17:59:26 -0300

    exer2

---
+++ Condicionais/exercico1.js
@@ -0,0 +1,13 @@
+const nota1 = 6;
+const nota2 = 9;
+const nota3 = 10;
+
+const media = (nota1 + nota2 + nota3)/3;
+
+if (media < 5){
+
+console.log("Reprovou")
+    }else if ( 5 >= media && media <= 7){
+
+    console.log("Recuperação")
+        }else { console.log("Passou de semestre")} 
